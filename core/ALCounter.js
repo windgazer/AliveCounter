311dba63633acd48d4f685d374dc1b76037deeab
Martin Reurings <martin@windgazer.nl>
2012-11-18 16:21:23 +0100

    Most counter related base-methods are in, now attempting to add loading of templates :)

--- core/ALCounter.js
+++ core/ALCounter.js
@@ -1,6 +1,55 @@
-var ALCounter = ( function() {
+window.windgazer = typeof window.windgazer == "undefined"? {}: window.windgazer;
+windgazer.ALCounterHelper = {};
+
+var ALCounter = ( function( domain ) {
 
 	var uidI = 0;
+
+	domain.ALCounterHelper = {
+			
+			queue: {},
+			
+			templates: {},
+
+			/**
+			 * Attempt to load an html-template into memory based on the
+			 * type of a 'component'. This method expects the file name
+			 * to match the type and the template to reside in the same
+			 * directory as well as having the same filename (with an
+			 * html extension instead of js...)
+			 */
+			loadTemplate : function( type ) {
+
+				var scripts = document.getElementsByTagName("script"),
+					l = scripts.length,
+					i = l,
+					re = new RegExp("^(.*/)" + type + ".js\\b.*$", "i");
+				
+				for (i; i--; ) {
+		
+					var s = scripts[i];
+					var url = s.src;
+					var m = url.match( re );
+					
+					if ( m ) {
+		
+						var templateUrl = m[1] + type + ".html";
+
+						if ( typeof this.templates[ templateUrl ] === "undefined"
+							 && typeof this.queue[ templateUrl ] === "undefined" ) {
+
+							console.log( "Setting new template loading to queue", templateUrl );
+							this,queue[ templateUrl ] = new HTTPRequest( null, false );
+
+						}
+		
+					}
+		
+				}
+
+			}
+			
+	};
 
 	function generateUID() {
 		return "ALCounter" + uidI++;
@@ -8,11 +57,23 @@ var ALCounter = ( function() {
 
 	return Class.extend({
 		init: function( params ) {
+			var params = params||{};
 			this.id = generateUID();
+			this.title = typeof params.title == 'undefined'?"No Title":params.title;
+			this.value = typeof params.value == 'undefined'?0:params.value;
 		},
 		getId: function() {
 			return this.id;
+		},
+		getType: function() {
+			throw "Must be implemented!!";
+		},
+		getTitle: function() {
+			return this.title;
+		},
+		getValue: function() {
+			return this.value;
 		}
 	});
 
-})();
+})( windgazer );
--- test/js/Counter.spec.js
+++ test/js/Counter.spec.js
@@ -1,16 +1,65 @@
 describe("ALCounter", function() {
+
 	it("exists", function() {
-		expect(ALCounter).toBeDefined();
+		expect( ALCounter ).toBeDefined();
 	});
+
 	it("can create a new one", function() {
 		var alc = new ALCounter();
-		expect(alc).toBeDefined();
-		expect(alc.getId().indexOf("ALCounter")).toBe(0);
+
+		expect( alc ).toBeDefined();
 	});
+
 	it("creates unique id's per instance", function() {
 		var alc1 = new ALCounter();
 		var alc2 = new ALCounter();
 		
-		expect(alc1.getId()).not.toEqual(alc2.getId());
+		expect( alc1.getId() ).not.toEqual( alc2.getId() );
 	});
+
+	it("throws an exception when getting type", function(){
+		var alc1 = new ALCounter();
+		var checkedException = function() {
+			alc1.getType();
+		}
+		
+		expect( checkedException ).toThrow();
+	});
+
+	it("sets default title to 'No Title'", function(){
+		var alc1 = new ALCounter();
+		
+		expect(alc1.getTitle()).toBe('No Title');
+	});
+	
+	it("sets title to provided title", function(){
+		var title = "My Counter";
+		var alc1 = new ALCounter( {title: title} );
+		
+		expect( alc1.getTitle() ).toBe( title );
+	});
+
+	it("sets default value to 0", function(){
+		var alc1 = new ALCounter();
+		
+		expect(alc1.getValue()).toBe( 0 );
+	});
+
+	it("sets value to provided value", function(){
+		var v = 42;
+		var alc1 = new ALCounter( {value: v} );
+		
+		expect( alc1.getValue() ).toBe( v );
+	});
+
+});
+
+describe("ALCounterHelper", function() {
+
+	it("loads a template for a type", function() {
+
+		windgazer.ALCounterHelper.loadTemplate("ALCounter");
+
+	});
+
 });
