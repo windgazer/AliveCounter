e6393886fabe6a51d0cb05f84f4eca10b27158fa
windgazer <martin@windgazer.nl>
2013-04-28 19:08:01 +0200

    Creating a build using grunt-usemin

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {            
+        release: {
+            files: {
+                'Release/': ['*.html','config.xml','fav*.*','assets/**/*','core/**/*','counters/**/*','libs/**/*','style/**/*']
+            }
+        }
+    },
+    clean: {
+        afterRelease: ['Release/libs']
+    },
+    useminPrepare: {
+        html: ['Release/index.html']
+    },
+    usemin: {
+        html: ['Release/index.html']
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Default task(s).
+  grunt.registerTask('default', ['copy:release', 'useminPrepare', 'concat', 'uglify', 'usemin','clean:afterRelease']);
+
+};
