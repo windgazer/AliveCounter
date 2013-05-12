c014a3be2a6677929a6554ee7e7f47666a25ae24
windgazer <martin@windgazer.nl>
2013-05-12 23:39:18 +0200

    Touch and Desktop compatible drag-drop library

---
+++ core/DragDrop.js
@@ -0,0 +1,92 @@
+var DragDrop = (function(body){
+
+    var DragDropMouseDown,
+        DragDropTouchDown,
+        DragDropMouseUp,
+        DragDropTouchUp,
+        handlers = {},
+        source = null,
+        handle = null,
+        promise = null;
+
+    function dragStart(e) {
+        var e = e||event,
+            target = e.target||e.srcElement,
+            dragSource = target,
+            dragHandle = null,
+            handler;
+        
+        while (dragSource && !(dragHandle = dragSource.getAttribute("data-dragsource"))){
+            dragSource = dragSource.parentNode;
+        }
+        
+        handler = handlers[dragHandle]
+
+        console.log(dragSource, dragHandle, handler);
+        if(dragSource && dragHandle && handler) {
+            handle = dragHandle;
+            source = dragSource;
+            //Create Promise
+            promise = new RSVP.Promise();
+            //Create .then()...
+            promise.then( function(dragTarget) {
+                //console.log(dragSource, dragTarget);
+                handler(dragSource,dragTarget);
+            });
+            //Create mouse/touch up handler to fullfill promise
+            DragDropMouseUp = events.attach( body, "mouseup", dragStop );
+            DragDropTouchUp = events.attach( body, "touchend", dragStop );
+        }
+        
+    }
+
+    function dragStop(e) {
+        DragDropMouseUp = events.detach(DragDropMouseUp);
+        DragDropTouchUp = events.detach(DragDropTouchUp);
+
+        var e = e||event,
+            target = e.target||e.srcElement,
+            dragTarget = target,
+            dragHandle = null;
+
+        if (e.changedTouches && document.elementFromPoint) {
+            dragTarget = document.elementFromPoint(
+                e.changedTouches[0].pageX,
+                e.changedTouches[0].pageY
+            );
+        }
+
+        while (
+            dragTarget &&
+            !(dragHandle = dragTarget.getAttribute("data-dragtarget"))
+        ){
+            dragTarget = dragTarget.parentNode;
+        }
+        
+        if(dragTarget && dragHandle && dragHandle === handle) {
+            promise.resolve(dragTarget);
+        } else {
+            promise.reject();
+        }
+    }
+
+    function init() {
+        DragDropMouseDown = events.attach( body, "mousedown", dragStart );
+        DragDropTouchDown = events.attach( body, "touchstart", dragStart );
+    }
+
+    function finish() {
+        DragDropMouseDown = events.detach( DragDropMouseDown );
+        DragDropTouchDown = events.detach( DragDropTouchDown );
+    }
+
+    return DragDrop = {
+        addHandler: function(handle, handler) {
+            handlers[handle] = handler;
+            if (!DragDropMouseDown) {
+                init();
+            }
+        }
+    };
+
+}(document.documentElement));
