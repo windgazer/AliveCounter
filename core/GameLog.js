4935a5f167d853919773e0c7265166bf0a039be2
windgazer <martin@windgazer.nl>
2013-05-18 17:15:23 +0200

    GameLogging now per 2 seconds any number of counters. Backwards compatible, but old code will only show first counter in log.

--- core/GameLog.js
+++ core/GameLog.js
@@ -2,91 +2,101 @@ window.windgazer = typeof window.windgazer == "undefined"? {}: window.windgazer;
 
 var GameLog = (function( domain ) {
 
-	var Logger = Class.extend( {
-		init: function() {
-			this.logs = new Array();
-			this.log("Start");
-		},
-		log: function( ) {
-			var now = new Date();
-			var entry = {
-					time: now,
-					content: arguments
-			};
-			this.logs.push( entry );
-			this.trigger( "log.modified", { data:entry } );
-		},
-		toString: function() {
-			var out = "";
-			for ( var i = 0; i < this.logs.length; i++ ) {
-				var entry = this.logs[i];
-				out += entry.time.toTimeString();
-				out += ": ";
-				for ( var j = 0; j < entry.content.length; j++ ) {
-					if ( j > 0 ) {
-						out += " ";
-					}
-					out += JSON.stringify( entry.content[j] );
-				}
-				out += "\n";
-			}
-			return out;
-		}
-	} );
-	
-	RSVP.EventTarget.mixin( Logger.prototype );
-
-	var logger = new Logger(),
-		timeout = false,
-		tuid = null,
-		ttotal = 0;
+    var Logger = Class.extend( {
+        init: function() {
+            this.logs = new Array();
+            this.log("Start");
+        },
+        log: function( ) {
+            var now = new Date(),
+                args = []; args.push.apply(args, arguments);
+            var entry = {
+                    time: now,
+                    content: args
+            };
+            this.logs.push( entry );
+            this.trigger( "log.modified", { data:entry } );
+        },
+        toString: function() {
+            var out = "";
+            for ( var i = 0; i < this.logs.length; i++ ) {
+                var entry = this.logs[i];
+                out += entry.time.toTimeString();
+                out += ": ";
+                for ( var j = 0; j < entry.content.length; j++ ) {
+                    if ( j > 0 ) {
+                        out += " ";
+                    }
+                    out += JSON.stringify( entry.content[j] );
+                }
+                out += "\n";
+            }
+            return out;
+        }
+    } );
+    RSVP.EventTarget.mixin( Logger.prototype );
+    
+    var promise = null,
+        logEntries = {},
+        logger = new Logger(),
+        timeout = false;
 
 	function resolveTimer() {
 
-		var cntr = domain.counters[tuid];
+        if ( timeout ) {
+            //Clear current timeout
+            window.clearTimeout( timeout );
+        }
+        timeout = false;
+        
+        var args = [],
+            i;
+        
+        for (i in logEntries) {
+            var uid = i,
+                entry = logEntries[uid],
+                cntr = domain.counters[uid];
+
+            args[args.length] = entry.mod;
+            args[args.length] = cntr.getValue();
+            args[args.length] = cntr.getTitle();
+
+        }
+
 		try {
-			logger.log( ttotal, cntr.getValue(), cntr.getTitle() );
-		} catch ( whatever ) { if ( console ) console.error( whatever ); }
-		tuid = null;
-		ttotal = 0;
-		timeout = false;
+			logger.log.apply( logger, args );
+		} catch ( whatever ) { if ( console ) console.error( args ); }
+		
+		logEntries = {};
+
+	}
+	
+	function logCounterModification( e ) {
+
+	    if ( timeout ) {
+            //Clear current timeout
+            window.clearTimeout( timeout );
+	    }
+	    //Set timeout
+        timeout = window.setTimeout( resolveTimer, 2000 );
+        
+        var uid = e.id,
+            entry = logEntries[uid]?logEntries[uid]:{ id: uid, mod: 0 };
+
+        entry.mod += e.inc;
+        
+        logEntries[uid] = entry;
 
 	}
 
 	GUIBuilder.on("gui.render", function( eventType, data ) {
 
+	    resolveTimer();
 		logger.log( "New Game" );
 
 	});
 
-	GUIBuilder.on("counter.modified", function( e ) {
-
-		if ( timeout ) {
-
-			//Clear current timeout
-			window.clearTimeout( timeout );
-
-			if ( tuid == e.id ) {
-
-				ttotal += e.inc;
-
-			} else {
-
-				resolveTimer();
-
-			}
-
-		}
-		if ( tuid=== null ) {
-
-			tuid = e.id;
-			ttotal = e.inc;
-
-		}
-		
-		timeout = window.setTimeout( resolveTimer, 3000 );
-
-	});
+	GUIBuilder.on("counter.modified", logCounterModification);
 	
 	function bubbleEvent( source, target, event ) {
 	    source.on( event, function( e ) {
