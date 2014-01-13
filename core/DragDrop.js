ba804faefb1745c87d96af234addd95497c758f0
windgazer <martin@windgazer.nl>
2014-01-13 22:58:30 +0100

    Yay, drag-drop life-steal is back!!

--- core/DragDrop.js
+++ core/DragDrop.js
@@ -8,20 +8,53 @@ var DragDrop = (function(body){
         handlers = {},
         source = null,
         handle = null,
-        promise = null;
+        promise = null,
+        getBody = ( function() {
+            var b = null;
+            
+            return function() {
+                if ( b === null ) {
+                    b = document.body;
+                }
+                return b;
+            };
+        }())
+    ;
+
+    function genericListenerMethod( type, method, add ) {
+        var b = getBody(),
+            lMethod = add?"addEventListener":"removeEventListener"
+        ;
+
+        b[lMethod]( type.toLowerCase(), method );
+        b[lMethod]( "MS" + type, method );
+    }
+
+    function addListener( type, method ) {
+
+        genericListenerMethod( type, method, true );
+
+    }
+
+    function removeListener( type, method ) {
+
+        genericListenerMethod( type, method, false );
+
+    }
 
     function dragStart(e) {
         var e = e||event,
             target = e.target||e.srcElement,
             dragSource = target,
             dragHandle = null,
-            handler;
+            handler
+        ;
         
         while (dragSource && !(dragHandle = dragSource.getAttribute("data-dragsource"))){
             dragSource = dragSource.parentNode;
         }
         
-        handler = handlers[dragHandle]
+        handler = handlers[dragHandle];
 
         if(dragSource && dragHandle && handler) {
             handle = dragHandle;
@@ -34,24 +67,27 @@ var DragDrop = (function(body){
                 handler(dragSource,dragTarget);
             });
             //Create mouse/touch up handler to fullfill promise
-            DragDropMouseUp = events.attach( body, "mouseup", dragStop );
-            DragDropTouchUp = events.attach( body, "touchend", dragStop );
+//            DragDropMouseUp = Events.attach( body, "mouseup", dragStop );
+//            DragDropTouchUp = Events.attach( body, "touchend", dragStop );
+            addListener( "PointerUp", dragStop );
             //For Android, must kill touchMove or no touchEnd is fired?!?
-            DragDropMove = events.attach( body, 'touchmove', function (e) { e.preventDefault(); });
+            DragDropMove = Events.attach( body, 'touchmove', function (e) { e.preventDefault(); });
 
         }
         
     }
 
     function dragStop(e) {
-        DragDropMouseUp = events.detach(DragDropMouseUp);
-        DragDropTouchUp = events.detach(DragDropTouchUp);
-        DragDropMove = events.detach(DragDropMove);
+//        DragDropMouseUp = Events.detach(DragDropMouseUp);
+//        DragDropTouchUp = Events.detach(DragDropTouchUp);
+        removeListener( "PointerUp", dragStop );
+        DragDropMove = Events.detach(DragDropMove);
 
         var e = e||event,
             target = e.target||e.srcElement,
             dragTarget = target,
-            dragHandle = null;
+            dragHandle = null
+        ;
 
         if (e.changedTouches && document.elementFromPoint) {
             dragTarget = document.elementFromPoint(
@@ -75,18 +111,23 @@ var DragDrop = (function(body){
     }
 
     function init() {
-        DragDropMouseDown = Events.attach( body, "mousedown", dragStart );
-        DragDropTouchDown = Events.attach( body, "touchstart", dragStart );
+//        DragDropMouseDown = Events.attach( body, "mousedown", dragStart );
+//        DragDropTouchDown = Events.attach( body, "touchstart", dragStart );
+        addListener( "PointerDown", dragStart );
         Events.attach( body, 'touchstart', function (e) { e.preventDefault(); });
         Events.attach( body, 'touchmove', function (e) { e.preventDefault(); });
     }
 
     function finish() {
-        DragDropMouseDown = Events.detach( DragDropMouseDown );
-        DragDropTouchDown = Events.detach( DragDropTouchDown );
+//        DragDropMouseDown = Events.detach( DragDropMouseDown );
+//        DragDropTouchDown = Events.detach( DragDropTouchDown );
+        removeListener( "PointerDown", dragStart );
     }
-    
-    init();
+
+
+    Events.attach( window, "load", function() {
+        init();
+    });
 
     return DragDrop = {
         addHandler: function(handle, handler) {
